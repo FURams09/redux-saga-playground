d976ab58e50f1bb19e27d11acafea5c0bae4b997
Greg <padin.greg@gmail.com>
2019-01-20 21:38:53 -0500

    Moved side effects to sagas

--- src/components/stations/StationList.js
+++ src/components/stations/StationList.js
@@ -37,55 +37,16 @@ const Marker = styled.div`
 class StationList extends Component {
   componentDidMount() {
     this.props.getStation();
-    axios
-      .get("https://gbfs.citibikenyc.com/gbfs/en/station_information.json")
-      .then(({ data }) => {
-        let { stations } = data.data;
-
-        const boroughStations = stations.map(station => {
-          let currentBorough = [];
-          if (false) { // set to false to prevent expensive filter before adding a local cache
-            currentBorough = boroughs.features.filter((borough) => {
-              return d3.geoContains(borough.geometry, [station.lon, station.lat]);
-            })
-
-          }
-          
-          let boroughName = 'N/A';
-          if (currentBorough.length > 0) { 
-            boroughName = currentBorough[0].properties.boro_name }
-          
-          return {
-            ...station,
-            borough: boroughName
-          }
-        })
-        
-        this.props.gotStation(boroughStations);
-      })
-      .catch(err => {
-        this.props.failedStation(err);
-      });
   }
 
   handleSort() {
-    let sortedStation = [...this.props.stations];
-    sortedStation.sort((a, b) => {
-        if (a.name < b.name) {
-          return -1
-        } else {
-          return 1
-        }
-      });
-     if (!this.props.sortAsc) {  
-      sortedStation = this.props.stations.reverse();
-    }
-    this.props.sortStations(sortedStation)
+    this.props.sortStations(this.props.stations, this.props.sortAsc)
   }
   render() {
     if (this.props.isLoading) {
       return <div>Loading...</div>;
     }
+    console.log(this.props.stations)
     const stations = this.props.stations.map((station, i) => {
       return <StationItem 
         key={i}
@@ -137,7 +98,7 @@ const mapDispatchToProps = dispatch => {
     getStation: () => dispatch({ type: RETRIEVE_STATIONS }),
     gotStation: payload => dispatch({ type: GOT_STATIONS, payload }),
     failedStation: err => dispatch({ type: FAILED_GET_STATIONS, payload: err }),
-    sortStations: (newStations) => dispatch({ type: SORT_STATIONS, payload: newStations }),
+    sortStations: (stations, isAsc) => dispatch({ type: SORT_STATIONS, payload: {stations, isAsc} }),
   };
 };
 
--- src/sagas/Stations.js
+++ src/sagas/Stations.js
@@ -1,3 +1,54 @@
-export default function* hello() {
-  console.log(Date.now())
-} 
+import axios from 'axios'
+
+import { put, takeEvery} from 'redux-saga/effects';
+import {RETRIEVE_STATIONS, GOT_STATIONS, FAILED_GET_STATIONS, SORT_STATIONS} from '../constants/stations'
+
+export default function* watchStation() {
+  yield takeEvery(RETRIEVE_STATIONS, getStationList);
+  yield takeEvery(SORT_STATIONS, sortStations)
+}
+
+function* getStationList() {
+  try {
+    const res = yield axios
+        .get("https://gbfs.citibikenyc.com/gbfs/en/station_information.json")
+    let { stations } = res.data.data;
+    const boroughStations = yield stations.map(station => {
+      let currentBorough = [];
+      // if (false) { // set to false to prevent expensive filter before adding a local cache
+      //   currentBorough = boroughs.features.filter((borough) => {
+      //     return d3.geoContains(borough.geometry, [station.lon, station.lat]);
+      //   })
+  
+      // }
+      
+      let boroughName = 'N/A';
+      if (currentBorough.length > 0) { 
+        boroughName = currentBorough[0].properties.boro_name }
+      
+      return {
+        ...station,
+        borough: boroughName
+      }
+    })
+          
+    yield put({type: GOT_STATIONS, payload: boroughStations});
+  } catch (error) {
+    yield put({type: FAILED_GET_STATIONS, payload: error})
+  }
+}
+
+function* sortStations(action) {
+  console.log(action.payload.stations)
+  let sortedStation = yield action.payload.stations.sort((a, b) => {
+    if (a.name < b.name) {
+      return -1
+    } else {
+      return 1
+    }
+  });
+ if (!action.payload.isAsc) {  
+    sortedStation = action.payload.stations.reverse();
+  }
+  yield put({type: GOT_STATIONS, payload: sortedStation});
+}
